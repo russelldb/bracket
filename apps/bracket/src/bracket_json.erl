a2608c04140a39f0f9aadd6a5f4b1511d586f145
Russell Brown <russell.brown@mac.com>
2010-12-21 10:26:41 +0000

    Json encoding of a tournament.

--- apps/bracket/src/bracket_json.erl
+++ apps/bracket/src/bracket_json.erl
@@ -9,8 +9,10 @@
 -module(bracket_json).
 
 %% API
--export([to_json/1, from_json/1]).
+%-export([to_json/1, from_json/1]).
 
+-include("bracket.hrl").
+-compile(export_all).
 %%%===================================================================
 %%% API
 %%%===================================================================
@@ -21,7 +23,7 @@
 %% @end
 %%--------------------------------------------------------------------
 to_json(T) ->
-    ok.
+    mochijson:encode(encode(T, [])).
 
 from_json(T) ->
     ok.
@@ -29,3 +31,21 @@ from_json(T) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+encode(undefined) ->
+    "";
+encode(T) when is_record(T, rider) ->
+    {struct, [{rider, {struct, [{name, T#rider.name}, {seed, T#rider.seed}]}}]};
+encode(T) when is_record(T, result) ->
+    Winner = encode(T#result.winner),
+    {struct, [{result, {struct, [{winner, Winner}, {time1, T#result.time1}, {time2, T#result.time2}]}}]};
+encode(T) when is_record(T, match) ->
+    {struct, [{match, {struct, [{number, T#match.number}, {rider1, encode(T#match.rider1)}, {rider2, encode(T#match.rider2)}, {result, encode(T#match.result)}]}}]};
+encode({round, N, {matches, M}}) ->
+    {struct, [{round, {struct, [{number, N}, {matches, encode(M, [])}]}}]}.
+
+
+encode([], Acc) ->
+    {array, lists:reverse(Acc)};
+encode([H|T], Acc) ->
+    encode(T, [encode(H)|Acc]).
+
