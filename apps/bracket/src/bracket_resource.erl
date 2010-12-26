62e8a5ece08bc7a2b990cdaca062cf688ee3c5e7
Russell Brown <russell.brown@mac.com>
2010-12-26 11:03:15 +0000

    Send bracket size.

--- apps/bracket/src/bracket_resource.erl
+++ apps/bracket/src/bracket_resource.erl
@@ -10,10 +10,26 @@
 init([]) -> {ok, undefined}.
 
 to_json(ReqData, State) ->
-    {bracket_json:to_json(bracket_tournament:tournament(bracket_test:generate(32))), ReqData, State}.
+    Size = get_qs_value("n", ReqData, 16),
+    try list_to_integer(Size) of
+	SizeInt ->
+	    {bracket_json:to_json(bracket_tournament:tournament(bracket_test:generate(SizeInt))), ReqData, State}
+    catch
+	error:badarg ->
+	    {{halt, 400}, ReqData, State}
+    end.
 
 content_types_provided(Req, Context) ->
     {[{"application/json", to_json}], Req, Context}.
 
 allowed_methods(Req, Context) ->
     {['GET'], Req, Context}.
+
+get_qs_value(Key, ReqData, Default) ->
+    get_qs_value(wrq:get_qs_value(Key, ReqData), Default).
+
+get_qs_value(undefined, Default) ->
+    Default;
+get_qs_value(Val, _Default) ->
+    Val.
+    
